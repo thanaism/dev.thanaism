a913c81c6fa03280aaf1dcd39cfeaad338f8c900
thanaism <62784012+thanaism@users.noreply.github.com>
2021-05-20 14:54:38 +0900

    emoji favicon

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -16,7 +16,8 @@ const IndexLayout: React.FC<IndexProps> = props => {
   return (
     <div className={props.className}>
       <Helmet>
-        <link rel="icon" href={favicon} type="image/x-icon" />
+        {/*<link rel="icon" href={favicon} type="image/x-icon" />*/}
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👁‍🗨</text></svg>" type="image/x-icon" />
       </Helmet>
       <Global
         styles={css`

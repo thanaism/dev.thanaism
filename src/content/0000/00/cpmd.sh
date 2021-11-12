835553b6f821338e1113b46d79fc9c7cf9701bef
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-11-13 02:15:31 +0900

    add templates and images

--- src/content/0000/00/cpmd.sh
+++ src/content/0000/00/cpmd.sh
@@ -4,6 +4,7 @@ do
     BN=$(basename $f)
     R=$RANDOM
     R=$(printf "%05d" "${R}")
+    touch tmp$R.md
     sed -e 's/img\//img\/'"$BN"'/g' template>>tmp$R.md
     mv $f ../../img/
     echo $COUNT;((COUNT++))
